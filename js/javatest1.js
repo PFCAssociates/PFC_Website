082b2302ad550b87a9c55aeb0f33ae3064369f39
PFCAssociates <pfcitadmin@pfcassociates.org>
2026-02-09 09:40:21 -0500

    Delete js directory

--- js/javatest1.js
+++
@@ -1,8 +0,0 @@
-// JUST FOR BACKGROUND CHANGE
-const toggle = document.querySelector('#toggle');
-
-const updateBackground = (event) => { 
-document.body.classList.toggle('on');
-}
-
-toggle.addEventListener("click", () => document.body.classList.toggle('on') , false);
